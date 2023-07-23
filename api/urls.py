2c421ee78d4f29480c23db3c486c4959d293976f
AldiyarIkhsan <usoul31@gmail.com>
2023-07-23 22:15:11 +0500

    added some fixes

--- api/urls.py
+++ api/urls.py
@@ -3,10 +3,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    # path('users/', views.UserList.as_view()),
-    # path('users//', views.UserDetail.as_view()),
-    # path('posts/', views.PostList.as_view()),
-    # path('posts//', views.PostDetail.as_view()),
-    # path('comments/', views.CommentList.as_view()),
-    # path('comments//', views.CommentDetail.as_view()),
+    path('users/', views.UserList.as_view()),
+    path('users//', views.UserDetail.as_view()),
+    path('posts/', views.PostList.as_view()),
+    path('posts//', views.PostDetail.as_view()),
+    path('comments/', views.CommentList.as_view()),
+    path('comments//', views.CommentDetail.as_view()),
 ]
